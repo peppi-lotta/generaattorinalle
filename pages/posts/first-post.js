3ca56676b898569a624a2486a1c609202f7c022b
Peppi-Lotta Saari <peppilotta.saari@gmail.com>
2022-10-14 09:29:12 +0300

    nalle-generator works perfectly

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,8 +2,69 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Nalle from './nalle';
 import Layout from '../../components/layout';
+import styles from '../../components/layout.module.scss';
+import { useState } from 'react'
+
+//if we get more nalle-parts just change the maximum value here
+var maximum = 5;
+var minimum = 1;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); 
+}
 
 export default function FirstPost() {
+  const [kuono, setKuono] = useState(getRandomInt(minimum, maximum));
+  const [masu, setMasu] = useState(getRandomInt(minimum, maximum));
+  const [korvat, setKorvat] = useState(getRandomInt(minimum, maximum));
+  const [kadet, setKadet] = useState(getRandomInt(minimum, maximum));
+  const [naama, setNaama] = useState(getRandomInt(minimum, maximum));
+  const [nena, setNena] = useState(getRandomInt(minimum, maximum));
+  const [silmat, setSilmat] = useState(getRandomInt(minimum, maximum));
+
+  function handleClick(type, state) {
+    let k;
+    if (state == maximum-1) {
+      k = 1;
+    } else {
+      k = state + 1
+    }
+    switch (type) {
+      case 'kuono':
+        setKuono(k);
+        break;
+      case 'masu':
+        setMasu(k);
+        break;
+      case 'korvat':
+        setKorvat(k);
+        break;
+      case 'kadet':
+        setKadet(k);
+        break;
+      case 'naama':
+        setNaama(k);
+        break;
+      case 'nena':
+        setNena(k);
+        break;
+      case 'silmat':
+        setSilmat(k);
+        break;
+      default: {
+        setKuono(getRandomInt(minimum, maximum));
+        setMasu(getRandomInt(minimum, maximum));
+        setKorvat(getRandomInt(minimum, maximum));
+        setKadet(getRandomInt(minimum, maximum));
+        setNaama(getRandomInt(minimum, maximum));
+        setNena(getRandomInt(minimum, maximum));
+        setSilmat(getRandomInt(minimum, maximum));
+      }
+    }
+  }
+
   return (
     <Layout>
       <Head>
@@ -15,7 +76,19 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-      <Nalle />
+      <div className={styles.nalle_control}>
+        <Nalle kuono={kuono} masu={masu} korvat={korvat} kadet={kadet} naama={naama} nena={nena} silmat={silmat} />
+        <div className={styles.buttons}>
+          <button onClick={() => handleClick()}>Random nalle</button>
+          <button onClick={() => handleClick('kuono', kuono)}>Vaihda kuono</button>
+          <button onClick={() => handleClick('masu', masu)}>Vaihda masu</button>
+          <button onClick={() => handleClick('korvat', korvat)}>Vaihda korvat</button>
+          <button onClick={() => handleClick('kadet', kadet)}>Vaihda kädet</button>
+          <button onClick={() => handleClick('naama', naama)}>Vaihda naama</button>
+          <button onClick={() => handleClick('nena', nena)}>Vaihda nenä</button>
+          <button onClick={() => handleClick('silmat', silmat)}>Vaihda silmät</button>
+        </div>
+      </div>
     </Layout>
   );
 }
--- pages/posts/nalle.js
+++ pages/posts/nalle.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Layout from '../../components/layout';
-import styles from '../../components/layout.module.css';
+import styles from '../../components/layout.module.scss';
 
 const NallePart = (props) => (
     <Image
@@ -11,30 +11,31 @@ const NallePart = (props) => (
     />
 );
 
-export default function Nalle() {
+export default function Nalle(props) {
     return (
         <Layout>
             <div className={styles.nalle}>
                 <div className={styles.nalle_part}>
-                    <NallePart imgName="/images/1_3.png" />
+                {props.kouno}
+                    <NallePart imgName={`/images/1_${props.kuono}.png`} />
                 </div>
                 <div className={styles.nalle_part}>
-                    <NallePart imgName="/images/2_1.png" />
+                    <NallePart imgName={`/images/2_${props.masu}.png`} />
                 </div>
                 <div className={styles.nalle_part}>
-                    <NallePart imgName="/images/3_3.png" />
+                    <NallePart imgName={`/images/3_${props.korvat}.png`} />
                 </div>
                 <div className={styles.nalle_part}>
-                    <NallePart imgName="/images/4_3.png" />
+                    <NallePart imgName={`/images/4_${props.kadet}.png`}  />
                 </div>
                 <div className={styles.nalle_part}>
-                    <NallePart imgName="/images/5_3.png" />
+                    <NallePart imgName={`/images/5_${props.naama}.png`}  />
                 </div>
                 <div className={styles.nalle_part}>
-                    <NallePart imgName="/images/6_1.png" />
+                    <NallePart imgName={`/images/6_${props.nena}.png`}  />
                 </div>
                 <div className={styles.nalle_part}>
-                    <NallePart imgName="/images/7_4.png" />
+                    <NallePart imgName={`/images/7_${props.silmat}.png`}  />
                 </div>
             </div>
         </Layout>
